644ee0563b20fceb433a10cf5cd13ed3d7c85bf8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-27 10:12:39 +0300

    Styled the task title

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,5 +1,16 @@
+import { Text } from '@chakra-ui/react'
+
 const Task = ({ title }) => {
-	return <div>Task</div>
+	return (
+		<Text
+			border={'1px solid'}
+			borderColor={'whiteAlpha.300'}
+			p={'0.5rem'}
+			textAlign={'center'}
+		>
+			{title}
+		</Text>
+	)
 }
 
 export default Task
