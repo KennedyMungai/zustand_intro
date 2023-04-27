c6331f6c839c36464047124e71f9e698ce476674
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-27 10:38:32 +0300

    Added some variants for the Tag component

--- src/theme.js
+++ src/theme.js
@@ -6,11 +6,21 @@ const theme = extendTheme({
 		'COLOR-GRAY-DARK': '#222',
 		'COLOR-GRAY-LIGHT': '#ccc',
 		'COLOR-ONGOING': '#98c1d9',
-		'COLOR-DONE': '#ee6c4d'
+		'COLOR-DONE': '#ee6c4d',
+		'bg-COLOR-PLANNED': '#FFAA44',
+		'bg-COLOR-ONGOING': '#FF6677',
+		'bg-COLOR-DONE': '#772288'
 	},
 	fonts: {
 		heading: 'roboto, sans-serif',
 		body: 'roboto, sans-serif'
+	},
+	Tag: {
+		variants: {
+			PLANNED: 'bg-COLOR-PLANNED',
+			ONGOING: 'bg-COLOR-ONGOING',
+			DONE: 'bg-COLOR-DONE'
+		}
 	}
 })
 
