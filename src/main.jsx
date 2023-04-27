483c393bf556b2dcb1acedad375220282215c491
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-27 09:45:36 +0300

    Applied the theme to the UI provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { ChakraProvider } from '@chakra-ui/react'
+import theme from './theme.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<ChakraProvider>
+	<ChakraProvider theme={theme}>
 		<React.StrictMode>
 			<App />
 		</React.StrictMode>
