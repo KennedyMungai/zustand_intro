a1fce10c62ba50ac524c2d3cde6bef184440fac1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-27 09:45:10 +0300

    Created the theme file for the app

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+	colors: {
+		'COLOR-GRAY': '#333',
+		'COLOR-GRAY-DARK': '#222',
+		'COLOR-GRAY-LIGHT': '#ccc',
+		'COLOR-ONGOING': '#98c1d9',
+		'COLOR-DONE': '#ee6c4d'
+	}
+})
+
+export default theme
